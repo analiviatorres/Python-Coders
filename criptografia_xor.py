b548c0452cb18080f4d52d76cfb1295bf876e122
analiviatorres <analiviatorres404@gmail.com>
2023-10-08 17:43:26 -0300

    adicionando comentários

--- criptografia_xor.py
+++ criptografia_xor.py
@@ -1,5 +1,5 @@
 nome_origem = (input("Digite uma palavra para criptografar: "))
-passe = (input("Digite agora uma palavra passe: "))
+passe = input("Digite agora uma palavra passe: ")
 destino = []
 
 if not passe:
@@ -8,14 +8,11 @@ else:
 
     for decripto, cripto in enumerate(nome_origem): #o enumerate obtem os indices das variáveis cripto e decripto
         n_origem = ord(cripto) #ord converte os caracteres da variável cripto para ascii e os armazena em n_origem
-        n_passe = ord(passe[decripto % len(passe)]) # vamos calcular e verificar se a palavra recebida em passe é menor que a recebida em nome_origem
+        n_passe = ord(passe[decripto % len(passe)]) # o "%" calcula o índice da palavra-passe, assim, garantindo que ela seja repetida quantas vezes necesária para criptografar todos os caracteres. 
+        # "len(passe)" retorna o comprimento da palavra-passe, garantindo que a palavra passe não seja menor que a palavra de origem.
         resultado = chr(n_origem ^ n_passe) # o chr retorna o resultado XOR em ASCII
         destino.append(resultado)
 
-        resultado_descriptografado = chr(ord(resultado) ^ n_passe)
-        resultado_completo_descriptografado = ''.join(resultado_descriptografado)
+resultado_criptografado = ''.join(destino)
 
-print(f'O resultado das duas palavras combinadas é: {destino}')
-print((f'Resultado descriptografado: {resultado_completo_descriptografado}'))
-
-#teste
+print(f'O resultado das duas palavras combinadas é: {resultado_criptografado}')
